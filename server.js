6730780a285cc9c38f56c2ab617a9de91020af52
Davie Nyirongo <bsc-inf-01-21@unima.ac.mw>
2026-05-16 17:25:07 +0200

    feat(infra): prepare for response compression middleware

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3006;
 
 app.use(cors());
 app.use(express.json());
+// app.use(compression()); // For response compression
 
 // Security Headers
 app.use((req, res, next) => {
